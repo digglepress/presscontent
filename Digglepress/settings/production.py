9a6445ddab280edc7a9dae6997e8a82d7cd4fe14
young baykay <youngtech412@gmail.com>
2020-04-14 04:49:08 +0100

    debugging deleted tinymce

--- Digglepress/settings/production.py
+++ Digglepress/settings/production.py
@@ -13,16 +13,6 @@ DATABASES = {
     }
 }
 
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# DEFAULT_FILE_STORAGE = config('DEFAULT_FILE_STORAGE')
-#
-# CLOUDINARY_STORAGE = {
-#     'CLOUD_NAME': config('CLOUD_NAME'),
-#     'API_KEY': config('API_KEY'),
-#     'API_SECRET': config('API_SECRET')
-# }
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
@@ -31,9 +21,3 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = '587'
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = config('EMAIL_HOST')
-# EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
-
